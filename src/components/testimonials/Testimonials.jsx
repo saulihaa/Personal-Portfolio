059083fa4d26ae150593024f86df360870b2f33c
saulihaa <114084643+saulihaa@users.noreply.github.com>
2023-04-04 18:49:34 +0530

    Deployed website

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -41,6 +41,14 @@ const data=[
   },
   {
     avatar:AVTR4,
+    name:'CSS Certificate',
+    by:'Certified By : HackerRank',
+    head: 'Skills Certification Test',
+    review:'It covers topics like exploring Cascading and Inheritance, exploring text styling fundamentals, understanding the use of layouts in CSS, understand the boxing of elements in CSS, among others.',
+    cert:'https://www.hackerrank.com/certificates/4651034b61d6'
+  },
+  {
+    avatar:AVTR4,
     name:'Problem Solving (Basic) Certificate ',
     by:'Certified By : HackerRank',
     head: 'Skills Certification Test',
@@ -48,20 +56,13 @@ const data=[
     cert:'https://www.hackerrank.com/certificates/5d0902642c39'
   },
 
-  {
-    avatar:AVTR4,
-    name:'CSS Certificate',
-    by:'Certified By : HackerRank',
-    head: 'Skills Certification Test',
-    review:'It covers topics like exploring Cascading and Inheritance, exploring text styling fundamentals, understanding the use of layouts in CSS, understand the boxing of elements in CSS, among others.',
-    cert:'https://www.hackerrank.com/certificates/5d0902642c39'
-  },
+
   {
     avatar:AVTR1,
     name:'The Complete HTML5 Course',
     by:'Offered By :  Creative Online School',
     head: 'WHAT I HAVE LEARNT:',
-    review:'It covers topics like to create amazing site using HTML5 , to work with Audio and Video in HTML5 ,Hands on experience on Data Input and Buttons,Hands on experience on Absolute and Relative Links,Hands on experience on Tags and Lists.' ,
+    review:'It covers topics like to create amazing site using HTML5,to work with Audio and Video in HTML5 ,Hands on experience on Data Input and Buttons,Hands on experience on Absolute and Relative Links and on Tags and Lists.' ,
     cert:'https://www.udemy.com/certificate/UC-d3618ab2-1f69-4fd3-83b2-ecf360cc0b7c/'
   },
   {
@@ -69,7 +70,7 @@ const data=[
     name:'Complete Modern JavaScript Firebase BootCamp',
     by:'Offered By :  Creative Online School',
     head: 'WHAT I HAVE LEARNT:',
-    review:'Go from a total beginner to an advanced JavaScript developer .Asynchronous JavaScript: The event loop, promises, async/await, AJAX and APIs .OOP including ES5 prototypes & ES6 classes .ES6 features like arrow functions, classes, default and rest parameters, etc.',
+    review:'It includes concepts like the event loop, promises, async/await, AJAX and APIs .OOP including ES5 prototypes & ES6 classes .ES6 features like arrow functions, classes, default and rest parameters, etc.',
     cert:'https://www.udemy.com/certificate/UC-94e9f1b3-6c01-4c96-bb0e-274b2e725e94/'
 
 
@@ -80,7 +81,7 @@ const data=[
   name:'Python And Flask Framework Complete',
   by:'Offered By :  Creative Online School',
   head: 'WHAT I HAVE LEARNT:',
-  review:'Write scripts for general productivity tasks Read and comprehend Python code Gain knowledge in regard to general programming concepts. Use variables to store, retrieve and calculate information,Utilize core programming tools.' ,
+  review:'Write scripts for general productivity tasks Read and comprehend Python code Gain knowledge in regard to general programming concepts. Use variables to store, retrieve and calculate information.' ,
   cert:'https://www.udemy.com/certificate/UC-849df06c-26e9-4492-b216-d3bc46ed70df/'
 },
 {
@@ -88,7 +89,7 @@ const data=[
   name:'The Complete Google Forms Course',
   by:'Offered By :  Creative Online School',
   head: 'WHAT I HAVE LEARNT:',
-  review:'Set up data filters and validations to better phrase questions. Interpret & analyze collected responses .Transform ordinary forms to quizzes for educational use .Distribute forms to any audience through multiple ways',
+  review:'Set up data filters and validations to better phrase questions,Interpret & analyze collected responses,Transform ordinary forms to quizzes for educational use,Distribute forms to any audience through multiple ways.',
   cert:'https://www.udemy.com/certificate/UC-4312a6b9-8ce6-4072-a921-4bbd71078d38/'
 }
 ]
